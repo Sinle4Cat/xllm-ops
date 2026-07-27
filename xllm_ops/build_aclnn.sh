6c6c569089090a56b7f0484518b5ef8bac8f9395
Super User <root@localhost.localdomain>
2026-07-27 10:28:28 +0800

    feat: support causal conv1d QKV on A5

--- xllm_ops/build_aclnn.sh
+++ xllm_ops/build_aclnn.sh
@@ -320,6 +320,7 @@ elif [[ "$SOC_VERSION" =~ ^ascend950 ]]; then
         "x_attention"
         "cache_unshared_kv"
         "causal_conv1d"
+        "causal_conv1d_qkv"
         "convert_kv_cache_format"
         "beam_search"
         "index_group_matmul"
